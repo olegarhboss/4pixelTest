fe3c98b7164e2967a20cf838c1751965bc772e46
Oleg Poliakov <webmasterolegan@gmail.com>
2020-07-23 23:33:10 +1000

    Базовая конфигурация

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+# Локализированные маршруты
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [
+        'localeSessionRedirect',
+        'localizationRedirect',
+        //'ServerPush'
+    ]], function () {
+
 });
